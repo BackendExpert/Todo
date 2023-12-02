a0275c60dacda761672748265b52830db764182a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-02 21:12:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ app/config/database.php
@@ -0,0 +1,11 @@
+<?php 
+    class database{
+        private $host = "localhost";
+        private $user = "root";
+        private $pass = "";
+        private $db_name = "todo_db";
+
+        
+    }
+
+?>
