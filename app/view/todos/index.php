b4c85f6f85296e0af142cdb29fc47a55705aa30f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-03 11:45:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/view/todos/index.php
+++ app/view/todos/index.php
@@ -3,9 +3,34 @@
     include_once("../../controller/TodoController.php");
 ?>
 
+<style>
+    .card{
+        margin: 20px 0; 
+    }
+    .areatext{
+        resize: none;
+        width: 100%;
+        height: 110px;
+    }
+</style>
 
 
 <div class="container">
+
+    <div class="card">
+        <div class="card-header">Add Todo</div>
+        <div class="card-body">
+            <form action="" method="POST">
+                <label for="topic">Topic : </label>
+                <input type="text" name="topic" id="" class="form-control"><br>
+
+                <label for="todo">Todo : </label>
+                <textarea name="todo" id="" class="form-control areatext"></textarea><br>
+
+                <input type="submit" value="Add Todo" name="add_todo" class="btn btn-success">
+            </form>
+        </div>
+    </div>
 
     <hr>
     <ol class="list-group list-group">
@@ -21,8 +46,8 @@
             <div class="fw-bold"><?= $todo['topic']; ?></div>
             <?= $todo['todo']; ?>
             </div>
-            <a href=""><button class="btn btn-warning btn-sm"><i class="fas fa-edit"></i>Edit</button></a> &nbsp;
-            <a href=""><button class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i>Delete</button></a>
+            <a href=""><button class="btn btn-warning btn-sm"><i class="fas fa-edit"></i> Edit</button></a> &nbsp;
+            <a href=""><button class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i> Delete</button></a>
             &nbsp;<span class="badge bg-primary rounded-pill"><?= $todo['add_date']; ?></span>
         </li>
               
