c7199e08ce280ac84c7d2e8b608b977df9976c97
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-03 10:57:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/config/database.php
+++ app/config/database.php
@@ -3,7 +3,7 @@
         private $host = "localhost";
         private $user = 'root';
         private $pass = '';
-        private $db_name = 'php_pdo_oop_crud';
+        private $db_name = 'todo_db';
 
         public function connect(){
             $con = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
