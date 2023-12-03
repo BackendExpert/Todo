8851450117a77b561b08ebc934228cc5c802b443
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-03 11:16:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/view/todos/index.php
+++ app/view/todos/index.php
@@ -4,15 +4,21 @@
 ?>
 
 <div class="container">
+    <div class="list-group">
     <?php 
         $todos = new TodoController();
         $all_todos = $todos->view_todos();
 
         foreach ($all_todos as $todo){
         ?>
-            
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1"><?= $todo['topic']; ?></h5>
+            <small>3 days ago</small>
+        </div>
+        <p class="mb-1"><?= $todo['todo']; ?></p>
         
         <?php
         }
     ?>
+    </div>
 </div>
