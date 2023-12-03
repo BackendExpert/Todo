196eaebb17cddcf4a3a117dd4d663c37fd894293
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-03 10:56:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/config/database.php
+++ app/config/database.php
@@ -1,11 +1,15 @@
 <?php 
-    class database{
+    class db{
         private $host = "localhost";
-        private $user = "root";
-        private $pass = "";
-        private $db_name = "todo_db";
+        private $user = 'root';
+        private $pass = '';
+        private $db_name = 'php_pdo_oop_crud';
 
-        
+        public function connect(){
+            $con = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+            $pdo = new PDO($con, $this->user, $this->pass);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $pdo;
+        }
     }
-
 ?>
