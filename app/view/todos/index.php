d09d1dd68d4f8f6ffd09884af4683c3972939248
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-03 11:22:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/view/todos/index.php
+++ app/view/todos/index.php
@@ -3,7 +3,16 @@
     include_once("../../controller/TodoController.php");
 ?>
 
+<style>
+    .list-group{
+        margin: 30px 0;
+    }
+
+</style>
+
 <div class="container">
+
+    <hr>
     <div class="list-group">
     <?php 
         $todos = new TodoController();
@@ -11,11 +20,14 @@
 
         foreach ($all_todos as $todo){
         ?>
+        <a href="#" class="list-group-item list-group-item-action" aria-current="true">
         <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1"><?= $todo['topic']; ?></h5>
             <small><?= $todo['add_date']; ?></small>
         </div>
         <p class="mb-1"><?= $todo['todo']; ?></p>
+        </a>
+        
         
         <?php
         }
