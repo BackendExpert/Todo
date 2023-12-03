cced33d68056cc2adb751e342c12ba592b348210
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-12-03 11:02:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app/model/TodoModel.php
+++ app/model/TodoModel.php
@@ -1,2 +1,14 @@
 <?php 
 
+    include("../config/database.php");
+
+    class TodoModel extends db {
+        public function all_todos(){
+            $sql = "SELECT * FROM todo_tbl";
+            $sql_exc = $this->connect()->prepare($sql);
+            $sql_exc->execute();
+            $result = $sql_exc->fetchAll();
+
+            return $result;
+        }
+    }
